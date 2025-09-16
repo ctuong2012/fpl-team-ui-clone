9dc9fc333252910d2ffc87ef2028a7e9d4d15656
ctuong2012 <ctuong2012@gmail.com>
2025-09-16 14:01:48 +0700

    Add App.js as main React component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to fpl-team-ui-clone!</h1>
+      <p>Your React app is running locally.</p>
+    </div>
+  );
+}
+
+export default App;
